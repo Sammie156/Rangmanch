f3f3d4a60eb61cc046632e37fdb935d10379772d
Sammie156 <samanwayadatta@gmail.com>
2025-09-08 02:25:11 +0530

    Started work on Backend

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,22 @@
-console.log("Hello from server");
-console.log("hello");
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server running at Port ${PORT}`);
+});
---
+++ backend/controllers/userController.js
@@ -0,0 +1,41 @@
+import pool from "../config/db.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const registerUser = async (req, res) => {
+    const {username, email, password} = req.body;
+
+    try {
+        const [existing] = await pool.query("SELECT * FROM users WHERE email = ?", [email]);
+        if (existing.length > 0) return res.status(400).json({message: "Email already in USE"});
+
+        const hash = await bcrypt.hash(password, 10);
+        const [result] = await pool.query(
+            "INSERT INTO users (username, email, password_hash) VALUES(? ? ?)", 
+            [username, email, hash]
+        );
+
+        res.status(201).json({id: result.insertId, username, email});
+    } catch(err) {
+        res.status(500).json({error: err.message});
+    }
+};
+
+export const loginUser = async (req, res) => {
+    const {username, password} = req.body;
+
+    try {
+        const [rows] = await pool.query("SELECT * FROM users WHERE username = ?", [username]);
+        if (rows.length === 0) return res.status(400).json({message: "Username not found"});
+
+        const user = rows[0];
+        const match = await bcrypt.compare(password, user.password_hash);
+
+        if (!match) return res.status(400).json({message: "Invalid Credentials"});
+
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn:"7d"});
+        res.json({token, user: {id: user.id, username: user.username, email: user.email}});
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+};
