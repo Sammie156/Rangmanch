1e4d821b4d4c50a853b875906f05d75b08cb81e2
Sammie156 <samanwayadatta@gmail.com>
2025-09-08 18:21:20 +0530

    Fixed SQL query. API works now for registering and logging in.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,40 +2,53 @@ import pool from "../config/db.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
+// WORKS FOR NOW
 export const registerUser = async (req, res) => {
-    const {username, email, password} = req.body;
+  const { username, email, password } = req.body;
 
-    try {
-        const [existing] = await pool.query("SELECT * FROM users WHERE email = ?", [email]);
-        if (existing.length > 0) return res.status(400).json({message: "Email already in USE"});
+  try {
+    const [existing] = await pool.query("SELECT * FROM users WHERE email = ?", [
+      email,
+    ]);
+    if (existing.length > 0)
+      return res.status(400).json({ message: "Email already in USE" });
 
-        const hash = await bcrypt.hash(password, 10);
-        const [result] = await pool.query(
-            "INSERT INTO users (username, email, password_hash) VALUES(? ? ?)", 
-            [username, email, hash]
-        );
+    const hash = await bcrypt.hash(password, 10);
 
-        res.status(201).json({id: result.insertId, username, email});
-    } catch(err) {
-        res.status(500).json({error: err.message});
-    }
+    const [result] = await pool.query(
+      "INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)",
+      [username, email, hash]
+    );
+
+    res.status(201).json({ id: result.insertId, username, email });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 export const loginUser = async (req, res) => {
-    const {username, password} = req.body;
+  const { username, password } = req.body;
 
-    try {
-        const [rows] = await pool.query("SELECT * FROM users WHERE username = ?", [username]);
-        if (rows.length === 0) return res.status(400).json({message: "Username not found"});
+  try {
+    const [rows] = await pool.query("SELECT * FROM users WHERE username = ?", [
+      username,
+    ]);
+    if (rows.length === 0)
+      return res.status(400).json({ message: "Username not found" });
 
-        const user = rows[0];
-        const match = await bcrypt.compare(password, user.password_hash);
+    const user = rows[0];
+    const match = await bcrypt.compare(password, user.password_hash);
 
-        if (!match) return res.status(400).json({message: "Invalid Credentials"});
+    if (!match) return res.status(400).json({ message: "Invalid Credentials" });
 
-        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn:"7d"});
-        res.json({token, user: {id: user.id, username: user.username, email: user.email}});
-    } catch (err) {
-        res.status(500).json({error: err.message});
-    }
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.json({
+      token,
+      user: { id: user.id, username: user.username, email: user.email },
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
