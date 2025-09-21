a9beb7c7392817605ebd385f4df3d73aff6fd896
Sammie156 <samanwayadatta@gmail.com>
2025-09-21 23:37:02 +0530

    Fixing frontend calling backend

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "https://rangmanch-site.netlify.app/"
+    ],
+    credentials: true
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 
